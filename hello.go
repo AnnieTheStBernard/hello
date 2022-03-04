dcb7dbd06e0c1802b91ade0229632c6cc64482af
Annie France <AnnieTheStBernard@gmail.com>
2022-03-04 10:22:04 -0700

    test more complicated exports

--- hello.go
+++ hello.go
@@ -3,3 +3,15 @@ package hello
 func Greeting() string {
 	return "Hello from go module"
 }
+
+func localOnly() string {
+	return "Since local is not capitalized this function cannot be called"
+}
+
+type GreetingType struct {
+	Hello string
+}
+
+func (p *GreetingType) GreetWithReceiver() string {
+	return localOnly()
+}
